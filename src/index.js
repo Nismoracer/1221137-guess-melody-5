8d3924be7204215479c4405507dce9d4b435b0fc
Илья Грунов <ilya.grunov@gmail.com>
2020-10-14 08:46:56 +0300

    Добавлены моковые данные. Реализовано динамическое отображение данныех.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import questions from "./mocks/questions";
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+console.log(questions);
+
 ReactDOM.render(
     <App
       errorsCount={Settings.ERRORS_COUNT}
+      questions={questions}
     />,
     document.querySelector(`#root`)
 );
